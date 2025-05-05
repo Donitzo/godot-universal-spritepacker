757715ab98ef6bfb30cbf153fcbdcda75e567af9
Donitzo <58847200+Donitzo@users.noreply.github.com>
2025-05-05 15:22:53 +0300

    Update run_packer.bat

--- sample/run_packer.bat
+++ sample/run_packer.bat
@@ -4,7 +4,7 @@ godot_universal_spritepacker^
     --source_directory "./test_sprites"^
     --spritesheet_path "./output/spritesheet"^
     --godot_resource_directory "res://textures"^
-    --godot_sprites_directory "./output/godot"^
+    --godot_sprites_directory "./output/godot/sprites"^
     --image_directory "./split_sprites"^
     --save_json
 
